55c57e32ae8b9f894f4cde2c65951c0496ed22c2
Avram Cosmin <info@sprindo.co.uk>
2020-10-25 19:18:05 +0200

    First commit.

---
+++ projects/ngx-helper/src/lib/ngx-helper.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { NgxHelperComponent } from './ngx-helper.component';
+
+
+
+@NgModule({
+  declarations: [NgxHelperComponent],
+  imports: [
+  ],
+  exports: [NgxHelperComponent]
+})
+export class NgxHelperModule { }
---
+++ projects/ngx-helper/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ngx-helper
+ */
+
+export * from './lib/ngx-helper.service';
+export * from './lib/ngx-helper.component';
+export * from './lib/ngx-helper.module';
