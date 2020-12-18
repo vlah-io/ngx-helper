bed24da583b256dabd3e92c8ac4344e28f7ddb27
Avram Cosmin <info@sprindo.co.uk>
2020-12-18 17:51:27 +0200

    Updated to Angular 11.0.5

--- projects/ngx-helper/src/lib/helper/array.helper.ts
+++ projects/ngx-helper/src/lib/helper/array.helper.ts
@@ -1,3 +1,5 @@
+import {ObjectHelper} from './object.helper';
+
 export class ArrayHelper {
   // https://stackoverflow.com/questions/5306680/move-an-array-element-from-one-array-position-to-another
   static move([...arr]: any[], oldIndex: number, newIndex: number): any[] {
@@ -8,5 +10,9 @@ export class ArrayHelper {
     arr.splice(newIndex, 0, arr.splice(oldIndex, 1)[0]);
 
     return arr;
+  }
+
+  static deepCopy<T>(target: T): T {
+    return ObjectHelper.deepCopy(target);
   }
 }
--- projects/ngx-helper/src/lib/helper/object.helper.ts
+++ projects/ngx-helper/src/lib/helper/object.helper.ts
@@ -72,4 +72,29 @@ export class ObjectHelper {
       '[object Number]'
     ].indexOf(type) !== -1;
   }
+
+  // https://gist.github.com/erikvullings/ada7af09925082cbb89f40ed962d475e
+  static deepCopy<T>(target: T): T {
+    if (target === null) {
+      return target;
+    }
+    if (target instanceof Date) {
+      return new Date(target.getTime()) as any;
+    }
+    if (target instanceof Array) {
+      const cp = [] as any[];
+      (target as any[]).forEach((v) => {
+        cp.push(v);
+      });
+      return cp.map((n: any) => ObjectHelper.deepCopy<any>(n)) as any;
+    }
+    if (typeof target === 'object' && target !== {}) {
+      const cp = {...(target as { [key: string]: any })} as { [key: string]: any };
+      Object.keys(cp).forEach(k => {
+        cp[k] = ObjectHelper.deepCopy<any>(cp[k]);
+      });
+      return cp as T;
+    }
+    return target;
+  }
 }
